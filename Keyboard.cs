ba78d17fdd9d26085a6e7003981a64f9c6e80f0f
Bailey <Anequit@gmail.com>
2021-07-24 08:35:32 -0400

    Refactored Codebase

--- Keyboard.cs
+++ Keyboard.cs
@@ -1,69 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using express_messenger.DataModels;
+using System;
 using System.Threading;
 using System.Windows.Forms;
 
 namespace express_messenger
 {
-    class Keyboard
+    static class Keyboard
     {
-        public static string Type(string message, int repeate, int delay, int initialDelay)
+        public static void Send(MessageData data) 
         {
-            if (message == null || message == "" || repeate == null || repeate == 0 || delay == null || delay == 0 || initialDelay == null || initialDelay == 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                return "An error occured, try again";
-            }
-            else
-            {
-                Console.WriteLine($"Keypresses will start firing in {initialDelay}s");
-                Thread.Sleep(initialDelay * 1000);
-                DateTime start = DateTime.UtcNow;
-                for (int i = 0; i < repeate; i++)
-                {
-                    if (i != repeate - 1)
-                    {
+            if (!data.Valid)
+                throw new Exception("Message data was invalid.");
+            
+            Console.WriteLine($"Keypresses will start firing in {data.InitialDelay} seconds.");
+            Thread.Sleep(data.InitialDelay * 1000);
 
-                        Thread.Sleep(delay);
-                        SendKeys.SendWait(message);
-                        SendKeys.SendWait("{Enter}");
-                        if (i == 0)
-                        {
-                            Console.WriteLine($"Sent {i + 1} time");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Sent {i + 1} times");
-                        }
-                    }
-                    else
-                    {
-                        Thread.Sleep(delay);
-                        SendKeys.SendWait(message);
-                        SendKeys.SendWait("{Enter}");
-                        if (i == 0)
-                        {
-                            Console.WriteLine($"Sent {i + 1} time");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Sent {i + 1} times");
-                        }
-                    }
-                }
-                if (repeate == 1)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    return $"Message sent successfully {repeate} time in {Convert.ToInt32((DateTime.UtcNow - start).TotalMilliseconds)}ms";
-                }
+            DateTime startTime;
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int x = 1; x <= data.Repeat; x++)
+            {
+                startTime = DateTime.Now;
+                Thread.Sleep(data.Delay);
+
+                SendKeys.SendWait(data.Message);
+                SendKeys.SendWait("{Enter}");
+
+                if (x == 1)
+                    Console.WriteLine($"Message sent successfully 1 time in {DateTime.Now.Subtract(startTime).TotalMilliseconds}ms");
                 else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    return $"Message sent successfully {repeate} times in {Convert.ToInt32((DateTime.UtcNow - start).TotalMilliseconds)}ms = Press Enter to conntinue";
-                }
+                    Console.WriteLine($"Message sent successfully {x} times in {DateTime.Now.Subtract(startTime).TotalMilliseconds}ms");
             }
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }

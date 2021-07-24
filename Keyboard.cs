470e8e7d896c395c78c3570babc3b780e24decfe
dk-raw <hello@dkatsios.tk>
2021-07-24 11:39:29 +0300

    Add project files.

---
+++ Keyboard.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace express_messenger
+{
+    class Keyboard
+    {
+        public static string Type(string message, int repeate, int delay, int initialDelay)
+        {
+            if (message == null || message == "" || repeate == null || repeate == 0 || delay == null || delay == 0 || initialDelay == null || initialDelay == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return "An error occured, try again";
+            }
+            else
+            {
+                Console.WriteLine($"Keypresses will start firing in {initialDelay}s");
+                Thread.Sleep(initialDelay * 1000);
+                DateTime start = DateTime.UtcNow;
+                for (int i = 0; i < repeate; i++)
+                {
+                    if (i != repeate - 1)
+                    {
+
+                        Thread.Sleep(delay);
+                        SendKeys.SendWait(message);
+                        SendKeys.SendWait("{Enter}");
+                        if (i == 0)
+                        {
+                            Console.WriteLine($"Sent {i + 1} time");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sent {i + 1} times");
+                        }
+                    }
+                    else
+                    {
+                        Thread.Sleep(delay);
+                        SendKeys.SendWait(message);
+                        SendKeys.SendWait("{Enter}");
+                        if (i == 0)
+                        {
+                            Console.WriteLine($"Sent {i + 1} time");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sent {i + 1} times");
+                        }
+                    }
+                }
+                if (repeate == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return $"Message sent successfully {repeate} time in {Convert.ToInt32((DateTime.UtcNow - start).TotalMilliseconds)}ms";
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return $"Message sent successfully {repeate} times in {Convert.ToInt32((DateTime.UtcNow - start).TotalMilliseconds)}ms = Press Enter to conntinue";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace express_messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string message;
+                int repeate, delay, initalDelay;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("* THE SOFTWARE IS PROVIDED AS IS WITHOUT ANY WARRANTY OR LIABILITY. \n* THE AUTHOR IS NOT RESPONSIBLE FOR ANY DAMAGES THAT THE PROGRAM MAY CAUSE. \n* USE AT YOUR OWN RISK AND USE YOUR HEAD PEOPLE!!!!");
+                Console.WriteLine("***************************************************************************");
+                Thread.Sleep(10000);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter the message");
+                message = Console.ReadLine();
+                Console.WriteLine("Specify how many times you want the message to be sent");
+                repeate = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Specify the delay between the messages in milliseconds");
+                delay = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Specify the initial delay(after how many seconds the program will start sending the messages)");
+                initalDelay = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Keyboard.Type(message, repeate, delay, initalDelay));
+                Console.ReadKey();
+            }
+        }
+    }
+}
